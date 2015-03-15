484cbae02a23cc984633f7d90514dcf660e38097
dmonhnt <dmonhnt.rit@gmail.com>
2015-03-15 14:47:06 -0700

    Updating hackmon.py with more functions

--- hackmon.py
+++ hackmon.py
@@ -48,9 +48,14 @@ def target_add(hackdb, target):
 	return;
 
 def keyword_add(hackdb, target_id, keyword):
-	hackdb.execute("insert into wordlist (target_id, keyword) values(%s, %s)", (target_id, keyword))
+	hackdb.execute("insert into keywords (target_id, keyword) values(%s, %s)", (target_id, keyword))
 	print "Number of Rows Updated: ", hackdb.rowcount
 	return;	
+
+def feed_add(hackdb, feed):
+	hackdb.execute("insert into sources (location, enabled) values(%s, %s)", (feed,1))
+	print "Number of Rows Updated: ", hackdb.rowcount
+	return;
 	
 def target_show(hackdb):
 	hackdb.execute("select * from targets")
@@ -60,20 +65,69 @@ def target_show(hackdb):
 		print row[0], row[2]
 	return;
 
+def keyword_show(hackdb, target_id):
+	hackdb.execute("select keyword from keywords where target_id = %s", [target_id])
+	rows = hackdb.fetchall()
+	print "keyword"
+	for row in rows:
+		print row[0]
+	return;
+
+def feed_show(hackdb):
+	hackdb.execute("select location from sources")
+	rows = hackdb.fetchall()
+	for row in rows:
+		print row[0]
+	return;
+
 def monitor(hackdb):
+	#get all enabled sources
 	hackdb.execute("select location from sources where enabled = 1")
 	srcs = hackdb.fetchall()
+
+	#pull down each feed (10 entries each) and parse
 	for src in srcs:
 		f = feedparser.parse(src[0])
+		
+		#parse through entries
 		for i in range(0,10):
         		date = f.entries[i].published
         		uid = hashlib.md5(f.entries[i].link).hexdigest()
         		link = f.entries[i].link
-       	 		data = f.entries[i].content
-			print date,uid,link,data
-			#check for base keywords
-				#if hit, load in DB & check for targets
-				#insert hit for each target
+       	 		data = f.entries[i].content[0].value
+			print "Checking: " + link
+			
+			#see whether entry matches base criteria
+			if is_hit(hackdb, -1, data):
+				print "--> Feed meets base criteria"
+				#save matching entry
+				save_file(hackdb, date, uid, link, data)
+
+				#check individual targets
+				hackdb.execute("select target_id from targets")
+				rows = hackdb.fetchall()
+				for row in rows:
+					#identify target hits
+					if is_hit(hackdb,row[0],data):
+						hackdb.execute("select target_name from targets where target_id=%s",[row[0]])
+						hits = hackdb.fetchone()
+						hit = hits[0]
+						print "Hit!!: "+hit[0]			
+	return;
+
+def is_hit(hackdb, target_id, content):
+        hackdb.execute("select keyword from keywords where target_id = %s", [target_id])
+        rows = hackdb.fetchall()
+	hit = False
+        for row in rows:
+                if row[0] in content:
+			print "--> Keyword "+row[0]+" matches!"
+                        hit = True
+        return hit;
+
+def save_file(hackdb, date, uid, link, data):
+	##insert the file
+	print "Saving File: "+link+ "UID: "+uid
 	return;
 
 #Parse the command line arguments
@@ -82,6 +136,9 @@ parser.add_argument('--check', help='Check the database connection', action='sto
 parser.add_argument('--tadd', help='Add a new target')
 parser.add_argument('--tshow', help='Show current targets', action="store_true", default=False)
 parser.add_argument('--fadd', help='Add a new feed')
+parser.add_argument('--fshow', help='Show current feeds', action="store_true", default=False)
+parser.add_argument("--kadd", help="Add a keyword for a given target", nargs=2)
+parser.add_argument("--kshow", help="Show keywords for a given target")
 parser.add_argument('--mon', help='Start monitoring', action='store_true', default=False)
 args = parser.parse_args()
 
@@ -95,8 +152,14 @@ elif args.tadd is not None:
 	target_add(hackdb,args.tadd)
 elif args.tshow:
 	target_show(hackdb)
-#elif fadd is not None:
-#	feed_add(hackdb,fadd)	
+elif args.fadd is not None:
+	feed_add(hackdb,args.fadd)
+elif args.fshow:
+	feed_show(hackdb)
+elif args.kadd is not None:
+	keyword_add(hackdb,args.kadd[0], args.kadd[1])
+elif args.kshow is not None:
+	keyword_show(hackdb,args.kshow)	
 elif args.mon:
 	monitor(hackdb)
 
