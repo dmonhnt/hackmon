38004f24ae4d0f7424987029db614f28706eb043
dmonhnt <dmonhnt.rit@gmail.com>
2015-03-15 17:51:59 -0700

    Adding hits and more accurate content checking

--- hackmon.py
+++ hackmon.py
@@ -6,6 +6,7 @@ import argparse
 import subprocess
 import feedparser
 import hashlib
+import datetime
 
 DBHOST = 'localhost'
 DBUSER = 'root'
@@ -80,39 +81,56 @@ def feed_show(hackdb):
 		print row[0]
 	return;
 
+def hits_show(hackdb, target):
+	hackdb.execute("select target_name, hit_time, link from hits join targets on hits.target_id = targets.target_id join files on files.file_id = hits.file_id where (%s = -1 or hits.target_id = %s)", (target,target))
+	rows = hackdb.fetchall()
+	print "target_name hit_time link"
+	for row in rows:
+		print row[0], row[1], row[2]
+	return;
+
 def monitor(hackdb):
 	#get all enabled sources
-	hackdb.execute("select location from sources where enabled = 1")
+	hackdb.execute("select location, source_id from sources where enabled = 1")
 	srcs = hackdb.fetchall()
 
 	#pull down each feed (10 entries each) and parse
 	for src in srcs:
+
+		print "Checking Feed: "+src[0]
 		f = feedparser.parse(src[0])
 		
 		#parse through entries
-		for i in range(0,10):
-        		date = f.entries[i].published
+		for i in range(0,len(f.entries)):
+        		
+			date = f.entries[i].published
         		uid = hashlib.md5(f.entries[i].link).hexdigest()
         		link = f.entries[i].link
-       	 		data = f.entries[i].content[0].value
-			print "Checking: " + link
+			data = ""
+			if "\'content\':" in str(f.entries[i]):
+				for c in range(0,len(f.entries[i].content)):
+					data = data+f.entries[i].content[c].value 
+			if "\'summary_detail\':" in str(f.entries[i]):
+				data = data+f.entries[i].summary_detail.value
+			if "\'summary\':" in str(f.entries[i]):
+				data = data+f.entries[i].summary
 			
+			print "Checking Entry: " + link
+
 			#see whether entry matches base criteria
 			if is_hit(hackdb, -1, data):
-				print "--> Feed meets base criteria"
-				#save matching entry
-				save_file(hackdb, date, uid, link, data)
-
-				#check individual targets
-				hackdb.execute("select target_id from targets")
-				rows = hackdb.fetchall()
-				for row in rows:
-					#identify target hits
-					if is_hit(hackdb,row[0],data):
-						hackdb.execute("select target_name from targets where target_id=%s",[row[0]])
-						hits = hackdb.fetchone()
-						hit = hits[0]
-						print "Hit!!: "+hit[0]			
+				print "\t--> Entry meets base criteria"
+				
+				if not file_exists(hackdb,uid):
+					#save matching entry	
+					fileid = save_file(hackdb, src[1], uid, link)
+					#check individual targets
+					hackdb.execute("select target_id from targets")
+					targets = hackdb.fetchall()
+					for target in targets:
+						#identify target hits
+						if is_hit(hackdb,target[0],data):
+							hit_add(hackdb, target[0], src[1], fileid)
 	return;
 
 def is_hit(hackdb, target_id, content):
@@ -121,14 +139,29 @@ def is_hit(hackdb, target_id, content):
 	hit = False
         for row in rows:
                 if row[0] in content:
-			print "--> Keyword "+row[0]+" matches!"
+			print "\t--> Keyword "+row[0]+" matches!"
                         hit = True
         return hit;
 
-def save_file(hackdb, date, uid, link, data):
+def save_file(hackdb, source_id, uid, link):
 	##insert the file
-	print "Saving File: "+link+ "UID: "+uid
+	hackdb.execute("insert into files (source_id, uid, link) values(%s,UNHEX(%s),%s)",(source_id, uid, link))
+	hackdb.execute("select file_id from files where uid = UNHEX(%s)", [uid])
+	fileid = hackdb.fetchone()
+	print "Saved File: "+"\n\t--> Source_ID: "+str(source_id)+"\n\t--> UID: "+str(uid)+"\n\t--> FileID: "+str(fileid[0])+"\n\t--> Link: "+str(link)
+	return fileid[0];
+
+def hit_add(hackdb, target_id, source_id, file_id):
+	print "--> HIT! TargetID: "+str(target_id)+" SourceID: " + str(source_id) + " FileID: " + str(file_id)
+	hackdb.execute("insert into hits (target_id, source_id, file_id) values(%s, %s, %s)", (target_id, source_id, file_id))
 	return;
+
+def file_exists(hackdb, uid):
+	exists = False
+	hackdb.execute("select * from files where uid = UNHEX(%s)", [uid])
+	if hackdb.rowcount > 0:
+		exists = True
+	return exists;
 
 #Parse the command line arguments
 parser = argparse.ArgumentParser()
@@ -139,6 +172,7 @@ parser.add_argument('--fadd', help='Add a new feed')
 parser.add_argument('--fshow', help='Show current feeds', action="store_true", default=False)
 parser.add_argument("--kadd", help="Add a keyword for a given target", nargs=2)
 parser.add_argument("--kshow", help="Show keywords for a given target")
+parser.add_argument("--hits", help="Show hits for a given target (-1 = all targets)")
 parser.add_argument('--mon', help='Start monitoring', action='store_true', default=False)
 args = parser.parse_args()
 
@@ -162,6 +196,8 @@ elif args.kshow is not None:
 	keyword_show(hackdb,args.kshow)	
 elif args.mon:
 	monitor(hackdb)
+elif args.hits is not None:
+	hits_show(hackdb, args.hits)
 
 #write changes to DB
 dbcon.commit()
